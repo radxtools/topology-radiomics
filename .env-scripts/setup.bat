155086a4abc06998b44e99b035ac6c569c0911e2
neshdev <dhineshdevanathan@gmail.com>
2020-08-25 21:54:10 -0400

    * Added plotly library * Added several notebooks with various composites : - both science and visualization frameworks * updated vscode launch config * updated setup.bat for windows vscode * working migration of matlab code to python * found bug with matlab code * Using pure python implementation of of python code

--- .env-scripts/setup.bat
+++ .env-scripts/setup.bat
@@ -1,3 +1,4 @@
 python -m venv .env
+python -m pip install --upgrade pip
 pip install -r dev_requirements.txt
 pip install -r ../requirements.txt
--- morphology/__init__.py
+++ morphology/__init__.py
@@ -2,14 +2,52 @@
 import numpy as np
 from numpy import gradient
 from collections import namedtuple
-from typing import NamedTuple
+from typing import NamedTuple, Tuple
 import scipy.ndimage as ndimage
 from skimage import measure
 import nibabel as nib
 from scipy.stats import describe
+import pyvista as pv
 
 # https://web.njit.edu/~rlopes/Mod5.2.pdf
 # https://docs.scipy.org/doc/numpy-1.13.0/reference/arrays.dtypes.html#arrays-dtypes-constructing
+
+
+def compute_morphology_features1(mri_voxels: np.ndarray, spacing: Tuple[float, float, float] = (1., 1., 1.)):
+
+    smoothed_mri_voxels = ndimage.gaussian_filter(mri_voxels, sigma=3)
+    verts, faces, normals, values = measure.marching_cubes(
+        smoothed_mri_voxels, spacing=spacing)
+    faces_rows = faces.shape[0]
+    poly_faces = np.column_stack(
+        [3*np.ones((faces_rows, 1), dtype=np.int), faces])
+    _isosurface = Isosurface(verts, faces, normals, values)
+    surface = pv.PolyData(verts, poly_faces.flatten())
+
+    mean_curvature = np.array(surface.curvature("Mean"))
+    gaussian_curvature = np.array(surface.curvature("Gaussian"))
+    p_min = np.array(surface.curvature("Minimum"))
+    p_max = np.array(surface.curvature("Maximum"))
+
+    _curvature = Curvature(
+        gaussian_curvature=gaussian_curvature,
+        mean_curvature=mean_curvature,
+        principal_curvature_min=p_min,
+        principal_curvature_max=p_max)
+
+    _curvedness = .5 * np.sqrt(p_min ** 2 + p_max**2)
+    _sharpness = (p_max - p_min)**2
+    _shape_index = 2/np.pi * np.arctan((p_max + p_min) / (p_max - p_min))
+    _total_curvature = np.abs(p_max) + np.abs(p_min)
+
+    _surface_measures = SurfaceMeasures(
+        curvedness=_curvedness, sharpness=_sharpness, shape_index=_shape_index, total_curvature=_total_curvature)
+
+    return MorphologyFeatures(
+        curvature=_curvature,
+        surface_measures=_surface_measures,
+        isosurface=_isosurface
+    )
 
 
 class Curvature(NamedTuple):
@@ -41,24 +79,25 @@ class SurfaceMeasures(NamedTuple):
 
     def __repr__(self):
         return (
-                f"Describe:\n"
-                f"Curvedness:{describe(self.curvedness.flatten(),nan_policy='omit')}\n"
-                f"Sharpness:{describe(self.sharpness.flatten(),nan_policy='omit')}\n"
-                f"Shape Index:{describe(self.shape_index.flatten(),nan_policy='omit')}\n"
-                f"Total Curvature:{describe(self.total_curvature.flatten(),nan_policy='omit')}\n"
+            f"Describe:\n"
+            f"Curvedness:{describe(self.curvedness.flatten(),nan_policy='omit')}\n"
+            f"Sharpness:{describe(self.sharpness.flatten(),nan_policy='omit')}\n"
+            f"Shape Index:{describe(self.shape_index.flatten(),nan_policy='omit')}\n"
+            f"Total Curvature:{describe(self.total_curvature.flatten(),nan_policy='omit')}\n"
 
-                f"Shapes:\n"
-                f"Curvedness:{self.curvedness.shape}\n"
-                f"Sharpness:{self.sharpness.shape}\n"
-                f"Shape Index:{self.shape_index.shape}\n"
-                f"Total Curvature:{self.total_curvature.shape}")
+            f"Shapes:\n"
+            f"Curvedness:{self.curvedness.shape}\n"
+            f"Sharpness:{self.sharpness.shape}\n"
+            f"Shape Index:{self.shape_index.shape}\n"
+            f"Total Curvature:{self.total_curvature.shape}")
 
 
 class Isosurface(NamedTuple):
-    verts : np.ndarray
+    verts: np.ndarray
     faces: np.ndarray
     normals: np.ndarray
     values: np.ndarray
+
 
 class MorphologyFeatures(NamedTuple):
     curvature: Curvature
@@ -70,7 +109,7 @@ class MorphologyFeatures(NamedTuple):
                 f"Surface measures:\n{self.surface_measures}")
 
 
-def compute_curvature(X, Y, Z):
+def compute_curvature(X: np.ndarray, Y: np.ndarray, Z: np.ndarray):
     """
     FootNotes:
     [1] Surface Curvature Function conversion in python: https://stackoverflow.com/questions/11317579/surface-curvature-matlab-equivalent-in-python
@@ -147,7 +186,9 @@ def compute_curvature(X, Y, Z):
                           principal_curvature_min=Pmin)
     return curvature
 
-#todo: what does dim do? looks like its scalaing the meshgrid?
+# todo: what does dim do? looks like its scalaing the meshgrid?
+
+
 def compute_morphology_features(mri_voxels: np.ndarray, dim: np.ndarray = np.array([1, 1, 1])):
     n2, n1, n3 = mri_voxels.shape
     d1, d2, d3 = dim[0], dim[1], dim[2]
@@ -164,7 +205,7 @@ def compute_morphology_features(mri_voxels: np.ndarray, dim: np.ndarray = np.arr
     verts, faces, normals, values = measure.marching_cubes(smoothed_mri_voxels)
     _isosurface = Isosurface(verts, faces, normals, values)
 
-    #cieling square for verticies??
+    # cieling square for verticies??
     dim = 0
     l = len(verts)
     for n in range(l//2):
@@ -200,58 +241,28 @@ def compute_morphology_features(mri_voxels: np.ndarray, dim: np.ndarray = np.arr
 
 
 def run(nii_path):
+    """
+    Wont have something like this in the final release (Used for testing)
+    """
     img = nib.load(nii_path)
     mri_3d_voxels = img.get_fdata()
     mri_3d_voxels[mri_3d_voxels > 0] = 255
     response = compute_morphology_features(mri_3d_voxels)
     return response
 
-# # %%
-# import numpy as np
-# from numpy import gradient
-# from collections import namedtuple
-# from typing import NamedTuple
-# import scipy.ndimage as ndimage
-# from skimage import measure
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-# # %%
-# A = np.ones((3,3,3))
-# T = np.zeros((5,5,5))
-# T[1:-1,1:-1,1:-1] = A
-# # %%
-# verts, faces, normals, values = measure.marching_cubes(T, 0)
-# fig = plt.figure(figsize=(100, 100))
-# ax = fig.add_subplot(111, projection='3d')
-
-# # Fancy indexing: `verts[faces]` to generate a collection of triangles
-# mesh = Poly3DCollection(verts[faces])
-# mesh.set_edgecolor('k')
-# ax.add_collection3d(mesh)
-# plt.tight_layout()
-# plt.show()
-# # %%
-
-# dim = 0
-# l=len(verts)
-# for n in range(l//2):
-#     if((n*n)>l):
-#         dim=n
-#         break
+def run1(nii_path):
+    """
+    Wont have something like this in the final release (Used for testing)
+    """
+    img = nib.load(nii_path)
+    mri_3d_voxels = img.get_fdata()
+    mri_3d_voxels[mri_3d_voxels > 0] = 255
+    response = compute_morphology_features1(mri_3d_voxels)
+    return response
 
 
-path = r"C:\Users\admin\bric-morphology\studies\1\data\Lesion.nii"
-d = run(path)
-print(d)
-
-
-# # %%
-# from itkwidgets import view
-
-# img = nib.load(path)
-# mri_3d_voxels = img.get_fdata()
-# mri_3d_voxels[mri_3d_voxels > 0] = 255
-
-# view(mri_3d_voxels)
+if __name__ == "__main__":
+    import sys
+    path = sys.argv[1]
+    run1(path)
